29c0b39259ca4f03a2d439e04fbcd9c953cccbbb
Ezard <ezard.ben@gmail.com>
2017-06-05 05:44:58 +0100

    Create homepage

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -2,10 +2,12 @@ import {BrowserModule} from "@angular/platform-browser";
 import {NgModule} from "@angular/core";
 
 import {AppComponent} from "./app.component";
+import {ImageCarouselComponent} from './image-carousel-component.component';
 
 @NgModule({
     declarations: [
-        AppComponent
+        AppComponent,
+        ImageCarouselComponent
     ],
     imports: [
         BrowserModule
