118f928f474e777895af37506387d419c7602e7e
Ezard <ezard.ben@gmail.com>
2017-06-05 06:26:11 +0100

    Move homepage data into its own set of files

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -2,11 +2,13 @@ import {BrowserModule} from "@angular/platform-browser";
 import {NgModule} from "@angular/core";
 
 import {AppComponent} from "./app.component";
+import {HomeComponent} from './home-component/home-component';
 import {ImageCarouselComponent} from './image-carousel/image-carousel-component';
 
 @NgModule({
     declarations: [
         AppComponent,
+        HomeComponent,
         ImageCarouselComponent
     ],
     imports: [
