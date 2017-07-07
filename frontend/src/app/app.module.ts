7a9b197c7cddf0e5eb7a17269d80a5f1de960f62
Ezard <ezard.ben@gmail.com>
2017-07-07 17:49:54 +0100

    Clean up namings for home section

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -5,7 +5,7 @@ import {NgModule} from "@angular/core";
 import {RouterModule} from "@angular/router";
 
 import {AppComponent} from "./app.component";
-import {HomeComponent} from "./home-component/home-component";
+import {HomeComponent} from "./home/home.component";
 import {AboutComponent} from "./about/about.component";
 import {AuthComponent} from "./auth/auth.component";
 import {ImageCarouselComponent} from "./image-carousel-component/image-carousel-component";
