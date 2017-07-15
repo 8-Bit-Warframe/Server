cf9cd123441f12cb76486d6f362af401d5321f15
Ezard <ezard.ben@gmail.com>
2017-07-15 12:27:17 +0100

    Add response structure

--- backend/src/routers/auth-router.ts
+++ backend/src/routers/auth-router.ts
@@ -1,4 +1,5 @@
 import {Router} from 'express';
+import {User} from '../../../shared/user';
 
 export class AuthRouter {
 
@@ -7,5 +8,17 @@ export class AuthRouter {
         });
         router.post('/register', (req, res) => {
         })
+    }
+}
+
+class AuthResponse {
+    success: boolean;
+    message: string;
+    user?: User;
+
+    constructor(success: boolean, message: string, user?: User) {
+        this.success = success;
+        this.message = message;
+        this.user = user;
     }
 }
