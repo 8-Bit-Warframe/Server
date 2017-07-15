241d4364ad8bb9334c28fb7513070112d763aaa2
Ezard <ezard.ben@gmail.com>
2017-07-15 12:26:05 +0100

    Add login/register method stubs

--- backend/src/routers/auth-router.ts
+++ backend/src/routers/auth-router.ts
@@ -3,5 +3,9 @@ import {Router} from 'express';
 export class AuthRouter {
 
     static addRoutes(router: Router): void {
+        router.post('/login', (req, res) => {
+        });
+        router.post('/register', (req, res) => {
+        })
     }
 }
