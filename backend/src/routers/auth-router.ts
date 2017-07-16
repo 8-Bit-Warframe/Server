b1bceabab1fc4dc4cc31e5699bc8dcd599aedc47
Ezard <ezard.ben@gmail.com>
2017-07-16 06:52:38 +0100

    Check that correct params are submitted

--- backend/src/routers/auth-router.ts
+++ backend/src/routers/auth-router.ts
@@ -1,13 +1,57 @@
-import {Router} from 'express';
+import {Request, Router} from 'express';
 import {User} from '../../../shared/user';
 
 export class AuthRouter {
+    private static paramNames = {
+        'alias': 'Alias',
+        'email': 'Email',
+        'password': 'Password',
+        'password2': 'Confirm Password'
+    };
 
     static addRoutes(router: Router): void {
         router.post('/login', (req, res) => {
+            let result = AuthRouter.checkQueryParams(req, ['email', 'password']);
+            console.log(result);
+            if (result === null) {
+                res.end();
+            } else {
+                res.send(new AuthResponse(false, result).toJsonString()).end();
+            }
         });
         router.post('/register', (req, res) => {
+            let result = AuthRouter.checkQueryParams(req, ['alias', 'email', 'password', 'password2']);
+            console.log(result);
+            if (result === null) {
+                res.end();
+            } else {
+                res.send(new AuthResponse(false, result).toJsonString()).end();
+            }
         })
+    }
+
+    private static checkQueryParams(req: Request, params: string[]): string {
+        let getName = (param) => AuthRouter.paramNames[param];
+        params.filter(element => !req.query[element]);
+        if (params.length > 0) {
+            if (params.length === 1) {
+                return `The field '${getName(params[0])}' must be filled in`;
+            } else {
+                let s = 'The fields ';
+                params.forEach((value, index, array) => {
+                    if (index < array.length - 2) {
+                        s += `'${getName(value)}', `;
+                    } else if (index < array.length - 1) {
+                        s += `'${getName(value)}' and `;
+                    } else {
+                        s += `'${getName(value)}' must be filled in`;
+                    }
+                });
+                return s;
+            }
+        } else {
+            return null;
+        }
     }
 }
 
@@ -20,5 +64,9 @@ class AuthResponse {
         this.success = success;
         this.message = message;
         this.user = user;
+    }
+
+    toJsonString(): string {
+        return JSON.stringify(this);
     }
 }
