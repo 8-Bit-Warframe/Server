2d91b56875b53947e7ac17fc4efaf6d4cedb2167
Ezard <ezard.ben@gmail.com>
2017-07-17 07:32:16 +0100

    Make Mongoose initialisation automatic

--- backend/src/config/mongoose.ts
+++ backend/src/config/mongoose.ts
@@ -1,13 +1,16 @@
 import * as mongoose from 'mongoose';
+import {Connection} from 'mongoose';
 
-export let setupMongoose = function(uri: string) {
-    (<any> mongoose).Promise = global.Promise;
-    mongoose.createConnection(uri)
-        .once('open', () => {
-            console.log('Mongoose connected to ' + uri);
-        })
-        .on('error', err => {
-            console.error("Mongoose failed to connect");
-            console.error(err);
-        });
-};
+const uri = 'mongodb://localhost/' + (process.env.LS_MONGODB || 'lsTest');
+
+export let db: Connection;
+
+(<any> mongoose).Promise = global.Promise;
+db = mongoose.createConnection(uri)
+             .once('open', () => {
+                 console.log('Mongoose connected to ' + uri);
+             })
+             .on('error', err => {
+                 console.error("Mongoose failed to connect");
+                 console.error(err);
+             });
--- backend/src/server.ts
+++ backend/src/server.ts
@@ -2,14 +2,11 @@ import * as express from 'express';
 import {Router} from 'express';
 import * as bodyParser from 'body-parser';
 import {AuthRouter} from './routers/auth-router';
-import {setupMongoose} from './config/mongoose'
 
 const app: express.Application = express();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
-
-setupMongoose('mongodb://localhost/' + (process.env.LS_MONGODB || 'lsTest'));
 
 const router: Router = express.Router();
 AuthRouter.addRoutes(router);
