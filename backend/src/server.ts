f9371a1e1534ed90a26ed8aa75f0c8e2fad89d78
Ezard <ezard.ben@gmail.com>
2017-07-19 09:29:40 +0100

    Allow cross-origin requests

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,5 +1,5 @@
 import * as express from 'express';
-import {Router} from 'express';
+import {NextFunction, Request, Response, Router} from 'express';
 import * as bodyParser from 'body-parser';
 import {AuthRouter} from './routers/auth-router';
 
@@ -7,6 +7,12 @@ const app: express.Application = express();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
+
+app.use((req: Request, res: Response, next: NextFunction) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
 
 const router: Router = express.Router();
 AuthRouter.addRoutes(router);
