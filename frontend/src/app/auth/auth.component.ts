6b9f86f3f4140ea4ed56b8c2c917c76966082161
Ezard <ezard.ben@gmail.com>
2017-07-19 12:30:56 +0100

    Implement registration and login

--- frontend/src/app/auth/auth.component.ts
+++ frontend/src/app/auth/auth.component.ts
@@ -1,11 +1,14 @@
 import {AfterViewInit, Component} from '@angular/core';
 
+import {AuthService} from './services/auth.service';
+
 declare const componentHandler: any;
 
 @Component({
     selector: 'auth',
     templateUrl: './auth.component.html',
-    styleUrls: ['./auth.component.scss']
+    styleUrls: ['./auth.component.scss'],
+    providers: [AuthService]
 })
 
 export class AuthComponent implements AfterViewInit {
@@ -14,6 +17,7 @@ export class AuthComponent implements AfterViewInit {
     password = '';
     password2 = '';
 
+    constructor(private authService: AuthService) {}
     selectTab(id: number): void {
         if (id === 0) {
             document.getElementById('login').classList.remove('is-active');
@@ -31,9 +35,25 @@ export class AuthComponent implements AfterViewInit {
         componentHandler.upgradeAllRegistered();
     }
     register() {
-        console.log(this.alias + ', ' + this.email + ', ' + this.password + ', ' + this.password2);
+        this.authService.register(this.alias, this.email, this.password, this.password2).then(value => {
+            if (value.success) {
+                window.localStorage.setItem('alias', value.user.alias);
+                window.localStorage.setItem('email', value.user.email);
+                window.localStorage.setItem('jwt', value.user.jwt);
+            } else {
+                // error
+            }
+        });
     }
     login() {
-        console.log(this.email + ', ' + this.password);
+        this.authService.login(this.email, this.password).then(value => {
+            if (value.success) {
+                window.localStorage.setItem('alias', value.user.alias);
+                window.localStorage.setItem('email', value.user.email);
+                window.localStorage.setItem('jwt', value.user.jwt);
+            } else {
+                // error
+            }
+        });
     }
 }
