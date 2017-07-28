241137578a49ffc69ef17fa95f89d0d4a7863c62
Ezard <ezard.ben@gmail.com>
2017-07-28 12:22:22 +0100

    Use different URLs depending on environment

--- frontend/src/app/auth/services/auth.service.ts
+++ frontend/src/app/auth/services/auth.service.ts
@@ -1,4 +1,4 @@
-import {Injectable} from '@angular/core';
+import {Injectable, isDevMode} from '@angular/core';
 import {Http, Response} from '@angular/http';
 
 import 'rxjs/add/operator/toPromise';
@@ -7,18 +7,20 @@ import {AuthResponse} from '../shared/auth-response';
 
 @Injectable()
 export class AuthService {
+    private baseUrl = isDevMode() ? 'http://localhost:3333' : 'https://api.8bitwarframe.com';
+
     constructor(private http: Http) {
     }
 
     register(alias: string, email: string, password: string, password2: string): Promise<AuthResponse> {
-        return this.http.post('http://localhost:3333/register', {alias: alias, email: email, password: password, password2: password2})
+        return this.http.post(`${this.baseUrl}/register`, {alias: alias, email: email, password: password, password2: password2})
                    .toPromise()
                    .then(AuthService.extractData)
                    .catch(console.error);
     }
 
     login(email: string, password: string): Promise<AuthResponse> {
-        return this.http.post('http://localhost:3333/login', {email: email, password: password})
+        return this.http.post(`${this.baseUrl}/login`, {email: email, password: password})
                    .toPromise()
                    .then(AuthService.extractData)
                    .catch(console.error);
