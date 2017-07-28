f8d69f91dd68d3202a94fb8978cdc303432e44f4
Ezard <ezard.ben@gmail.com>
2017-07-28 10:55:19 +0100

    Add link to profile page, and ability to logout

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import {Component} from '@angular/core';
+import {StorageService} from './services/storage.service';
 
 @Component({
     selector: 'app-root',
@@ -6,4 +7,20 @@ import {Component} from '@angular/core';
     styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+    private loggedIn: boolean;
+
+    constructor(private storageService: StorageService) {
+        storageService.subscribe((key, value) => {
+            if (key === 'jwt') {
+                this.loggedIn = value && value.length > 0;
+            }
+        });
+        this.loggedIn = storageService.jwt && storageService.jwt.length > 0;
+    }
+
+    logout() {
+        this.storageService.alias = '';
+        this.storageService.email = '';
+        this.storageService.jwt = '';
+    }
 }
