c01ffe77e0a03f6896240b1cbefb9cdb924b50c7
Ezard <ezard.ben@gmail.com>
2017-07-28 19:33:21 +0100

    Add service for getting and adding friends

---
+++ frontend/src/app/profile/services/profile.service.ts
@@ -0,0 +1,35 @@
+import {Injectable, isDevMode} from '@angular/core';
+import {Headers, Http} from '@angular/http';
+
+import 'rxjs/add/operator/toPromise';
+import {StorageService} from '../../services/storage.service';
+
+@Injectable()
+export class ProfileService {
+    private baseUrl = isDevMode() ? 'http://localhost:3333' : 'https://api.8bitwarframe.com';
+    private headers = new Headers();
+
+    constructor(private http: Http, private storageService: StorageService) {
+        storageService.subscribe((key, value) => {
+            if (key === 'jwt') this.onJwtChange(value);
+        });
+        this.onJwtChange(storageService.jwt);
+    }
+
+    private onJwtChange(jwt: string) {
+        this.headers.set('Authorization', `Bearer ${jwt}`);
+    }
+
+    addFriend(alias: string) {
+        return this.http.post(`${this.baseUrl}/user/friends`, {alias: alias}, {headers: this.headers})
+                   .toPromise()
+                   .catch(console.error);
+    }
+
+    getFriends(): Promise<any> {
+        return this.http.get(`${this.baseUrl}/user/friends`, {headers: this.headers})
+                   .toPromise()
+                   .then(value => value.json())
+                   .catch(console.error);
+    }
+}
