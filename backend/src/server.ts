f25bbccdcc449cdbfa01db8fdfa16fbdc1a740c2
Ezard <ezard.ben@gmail.com>
2017-07-28 14:53:34 +0100

    Allow Authorization header

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -11,7 +11,7 @@ app.use(bodyParser.urlencoded({extended: true}));
 
 app.use((req: Request, res: Response, next: NextFunction) => {
     res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
     next();
 });
 
